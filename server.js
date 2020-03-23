fd738322dbd32281078fceee085e466c4e0a20f8
Florian Maisonnave <florian.maisonnave@epitech.eu>
2020-03-23 17:55:56 +0100

    Regen db model

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var apiRouter = require('./apiRouter').router;
-
 var server = express();
 
-server.use(bodyParser.urlencoded({extended : true}));
-server.use(bodyParser.json());
-
-// Routes
-server.get('/', function(req, res) {
-    res.status(200).send('<p>Test</p>');
-    res.setHeader('Content-Type', 'text/html');
-});
-
-server.use('/ api/', apiRouter);
+server.get('/', function (req, res) {
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send('<h1>Linkedin API</h1>');
+})
 
 server.listen(8080, function() {
-    console.log('Server launch');
-})
+  console.log('Server start');
+});
